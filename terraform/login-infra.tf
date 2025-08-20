785ea39ab912b4a1a94bd0e50b774de42b6b8eee
saidinesh31155 <saidinesh31155@gmail.com>
2025-08-20 16:18:57 +0530

    create api and db subnets

--- terraform/login-infra.tf
+++ terraform/login-infra.tf
@@ -20,3 +20,19 @@ resource "azurerm_subnet" "web-sn" {
   address_prefixes     = ["10.0.1.0/24"]
 } 
 
+#api subnet
+resource "azurerm_subnet" "api-sn" {
+  name                 = "api-subnet"
+  resource_group_name  = azurerm_resource_group.login-rg.name
+  virtual_network_name = azurerm_virtual_network.login-vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+} 
+
+#db subnet
+resource "azurerm_subnet" "db-sn" {
+  name                 = "db-subnet"
+  resource_group_name  = azurerm_resource_group.login-rg.name
+  virtual_network_name = azurerm_virtual_network.login-vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+} 
+
