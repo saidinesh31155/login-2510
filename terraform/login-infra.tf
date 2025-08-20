192952da273ea2eab4e1d4737b7e9b7d4bdddbee
saidinesh31155 <saidinesh31155@gmail.com>
2025-08-20 22:20:21 +0530

    create db nic with nsg asc

--- terraform/login-infra.tf
+++ terraform/login-infra.tf
@@ -213,6 +213,25 @@ resource "azurerm_network_interface_security_group_association" "login-api-nic-n
   network_security_group_id = azurerm_network_security_group.api-nsg.id
 }
 
+#db nic
+resource "azurerm_network_interface" "db-nic" {
+  name                = "login-db-nic"
+  location            = azurerm_resource_group.login-rg.location
+  resource_group_name = azurerm_resource_group.login-rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.db-sn.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+#db nic nsg asc
+resource "azurerm_network_interface_security_group_association" "login-db-nic-nsg-asc" {
+  network_interface_id      = azurerm_network_interface.db-nic.id
+  network_security_group_id = azurerm_network_security_group.db-nsg.id
+}
+
 
 
 
