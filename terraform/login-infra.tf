e504c7e8401bb246bdd8ad760679b4bb060dfe98
saidinesh31155 <saidinesh31155@gmail.com>
2025-08-20 21:50:06 +0530

    create db nsg rules

--- terraform/login-infra.tf
+++ terraform/login-infra.tf
@@ -143,4 +143,36 @@ resource "azurerm_network_security_rule" "api-http" {
   network_security_group_name = azurerm_network_security_group.api-nsg.name
 }
 
+#db ssh
+resource "azurerm_network_security_rule" "db-ssh" {
+  name                        = "login-db-ssh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.login-rg.name
+  network_security_group_name = azurerm_network_security_group.db-nsg.name
+}
+
+#db http
+resource "azurerm_network_security_rule" "db-http" {
+  name                        = "login-db-http"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "5432"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.login-rg.name
+  network_security_group_name = azurerm_network_security_group.db-nsg.name
+}
+
+
+
 
