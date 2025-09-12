c70d19a487f805b67cccd5cbc702334993db2d67
saidinesh31155 <saidinesh31155@gmail.com>
2025-09-12 16:29:23 +0530

    updated

--- terraform/login-infra.tf
+++ terraform/login-infra.tf
@@ -86,6 +86,9 @@ resource "azurerm_network_interface" "nic" {
   location            = azurerm_resource_group.login-rg.location
   resource_group_name = azurerm_resource_group.login-rg.name
 
+   # Attach NSG to NIC
+  network_security_group_id = azurerm_network_security_group.network_security_groups[each.value.nsg_key].id
+
   ip_configuration {
     name                          = "internal"
 
