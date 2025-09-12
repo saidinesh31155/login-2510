cf1d154e2ab6b862bcf8a70ec37c62f8d545833d
saidinesh31155 <saidinesh31155@gmail.com>
2025-09-12 15:00:35 +0530

    updated

--- terraform/login-infra.tf
+++ terraform/login-infra.tf
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network" "login-vnet" {
   name = var.vnet_name
   location = azurerm_resource_group.login-rg.location
   resource_group_name = azurerm_resource_group.login-rg.name
-  address_space = var.vnet_address_space 
+  address_space = [var.vnet_address_space]
 }
 
 #Public subnets
