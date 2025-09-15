27f1ef967ac27f13674caf345b89d800940b7421
saidinesh31155 <saidinesh31155@gmail.com>
2025-09-15 14:33:58 +0530

    updated

--- proj_modules/modules/vnet/main.tf
+++ proj_modules/modules/vnet/main.tf
@@ -34,8 +34,8 @@ resource "azurerm_public_ip" "public_ip_name" {
 }
 
 #network security group
-resource "azurerm_network_security_group" "web_nsg" {
-  name = "frontend-nsg"
+resource "azurerm_network_security_group" "web-nsg" {
+  name = "web-nsg"
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
