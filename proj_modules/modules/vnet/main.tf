e4c116222045b4e55151b18dca15194bd889b828
saidinesh31155 <saidinesh31155@gmail.com>
2025-09-15 13:00:54 +0530

    updated

--- proj_modules/modules/vnet/main.tf
+++ proj_modules/modules/vnet/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network" "vnet" {
   name = var.vnet_name
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space = var.vnet_address_space
+  address_space = ["10.0.0.0/16"]
 }
 
 #Public subnet
