57e06d0dc8fa7872200567359d74759737e236bb
saidinesh31155 <saidinesh31155@gmail.com>
2025-09-15 12:13:03 +0530

    config vm file

---
+++ proj_modules/modules/vm/main.tf
@@ -0,0 +1,27 @@
+#linux vm
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = var.rg_name
+  location            = var.rg_location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  custom_data         = filebase64("var.custom_data_path")
+  network_interface_ids = [var.network_interface_id]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = var.os_disk_type
+  }
+
+  source_image_reference {
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
+  }
+}
---
+++ proj_modules/modules/vm/variables.tf
@@ -0,0 +1,63 @@
+#variables for vnet components
+variable "vm_name" {
+  description = "please input vm name"
+}
+
+variable "rg_name" {
+  description = "please input resource group name"
+}
+
+variable "rg_location" {
+  description = "please input resource group location"
+}
+
+
+variable "vm_size" {
+  description = "please input vm size"
+  default     = "Standard_F2"
+}
+
+variable "admin_username" {
+  description = "please input admin_username"
+  default     = "ubuntu"
+}
+
+variable "custom_data_path" {
+  description = "Path to custom data script"
+}
+
+variable "ssh_public_key_path" {
+  type        = string
+  description = "Path to SSH public key"
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "network_interface_id" {
+  type        = string
+  description = "ID of the network interface"
+}
+
+variable "os_disk_type" {
+  type        = string
+  default     = "Standard_LRS"
+}
+
+variable "image_publisher" {
+  type        = string
+  default     = "Canonical"
+}
+
+variable "image_offer" {
+  type        = string
+  default     = "0001-com-ubuntu-server-jammy"
+}
+
+variable "image_sku" {
+  type        = string
+  default     = "22_04-lts"
+}
+
+variable "image_version" {
+  type        = string
+  default     = "latest"
+}
